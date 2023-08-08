a5a2ea75b094dab6eaf4ff1633dd2b8c41754380
Marcus Moore <contact@marcusmoore.io>
2023-08-08 15:54:06 -0700

    Add test around user count on dashboard

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Company;
 use App\Models\User;
 use Tests\Support\InteractsWithSettings;
 use Tests\TestCase;
@@ -15,5 +16,43 @@ class DashboardTest extends TestCase
         $this->actingAs(User::factory()->create())
             ->get(route('home'))
             ->assertRedirect(route('view-assets'));
+    }
+
+    public function testUserCountForDashboard()
+    {
+        $companyA = Company::factory()->create();
+        $companyB = Company::factory()->create();
+
+        // Company A has 2 users
+        $superUser = $companyA->users()->save(User::factory()->superuser()->make());
+        $companyA->users()->saveMany(User::factory()->count(1)->make());
+
+        // Company B has 3 users
+        $adminUser = $companyB->users()->save(User::factory()->admin()->make());
+        $companyB->users()->saveMany(User::factory()->count(2)->make());
+
+        $this->settings->disableMultipleFullCompanySupport();
+
+        $this->actingAs($superUser)
+            ->get(route('home'))
+            ->assertOk()
+            ->assertViewHas('counts', fn($counts) => $counts['user'] === 5);
+
+        $this->actingAs($adminUser)
+            ->get(route('home'))
+            ->assertOk()
+            ->assertViewHas('counts', fn($counts) => $counts['user'] === 5);
+
+        $this->settings->enableMultipleFullCompanySupport();
+
+        $this->actingAs($superUser)
+            ->get(route('home'))
+            ->assertOk()
+            ->assertViewHas('counts', fn($counts) => $counts['user'] === 5);
+
+        $this->actingAs($adminUser)
+            ->get(route('home'))
+            ->assertOk()
+            ->assertViewHas('counts', fn($counts) => $counts['user'] === 3);
     }
 }
