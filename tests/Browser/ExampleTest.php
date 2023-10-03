36096fbfb3c700729ebf4d6da80cd1eb32482c13
Marcus Moore <contact@marcusmoore.io>
2023-10-03 16:38:32 -0700

    Add a login test

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -2,16 +2,32 @@
 
 namespace Tests\Browser;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
 class ExampleTest extends DuskTestCase
 {
-    public function testBasicExample(): void
+    public function testCanSeeLoginPage()
     {
         $this->browse(function (Browser $browser) {
-            $browser->visit('/login')
-                ->assertSee('Login');
+            $browser->visit(route('login'))
+                ->assertSee(trans('auth/general.login_prompt'));
+        });
+    }
+
+    public function testCanLogin()
+    {
+        $user = User::factory()->create(['password' => Hash::make('password')]);
+
+        $this->browse(function (Browser $browser) use ($user) {
+            $browser->visitRoute('login')
+                ->type('username', $user->username)
+                ->type('password', 'password')
+                ->press(trans('auth/general.login'))
+                ->assertPathIs('/account/view-assets')
+                ->screenshot('dashboard');
         });
     }
 }
