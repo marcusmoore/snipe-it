55e2264b13a809dc6b2461b1d79879de7a8cd364
Marcus Moore <contact@marcusmoore.io>
2023-10-05 17:04:36 -0700

    WIP: Continue working on asset checkout test

--- tests/Browser/AssetCheckoutTest.php
+++ tests/Browser/AssetCheckoutTest.php
@@ -6,22 +6,37 @@ use App\Models\Asset;
 use App\Models\User;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
+use Tests\Support\InteractsWithAuthentication;
 
 class AssetCheckoutTest extends DuskTestCase
 {
-    public function testExample(): void
+    use InteractsWithAuthentication;
+
+    public function testCanCheckoutAssetToUser(): void
     {
         $this->markTestIncomplete();
 
-        $user = User::factory()->checkoutAssets()->create();
+        $admin = User::factory()->superuser()->create();
+        $user = User::factory()->superuser()->create();
         $asset = Asset::factory()->create();
 
-        $this->browse(function (Browser $browser) use ($asset, $user) {
-            $browser->loginAs($user)
+        // @todo remove this: set up to see if "Assets currently checked out to this user" populates. it does not.
+        Asset::factory()->create()->checkOut($user, $admin);
+
+        $this->browse(function (Browser $browser) use ($user, $asset, $admin) {
+            $browser->loginAs($admin)
                 ->visit(route('hardware.checkout.create', $asset->id))
                 ->assertSee(trans('admin/hardware/general.checkout'))
                 ->type('name', 'Changed Asset Name')
-                ->click('@checkout-selector-location')
+                ->click('@checkout-selector-user')
+                ->tap(function ($browser) use ($user) {
+                    // @todo: this doesn't work since select2 sends request to api after selection
+                    $browser->script([
+                        "$('#assigned_user_select').val('{$user->id}')",
+                        "$('#assigned_user_select').trigger('change')",
+                    ]);
+                })
+                // ->click('@asset-checkout-submit-button')
                 ->stop();
         });
     }
