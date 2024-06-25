cb379eceb1f82797f162bd4b48781ce381f856f6
Marcus Moore <contact@marcusmoore.io>
2024-06-25 16:08:00 -0700

    Add additional test cases

--- tests/Feature/Throwaway.php
+++ tests/Feature/Throwaway.php
@@ -11,15 +11,63 @@ class Throwaway extends TestCase
 {
     protected static function provider()
     {
-        yield 'Admin attempting to update user without a company' => Provider::data(function () {
+        yield 'Admin for one company should be allowed to update user from same company' => Provider::data(function () {
+            $company = Company::factory()->create();
+
+            return [
+                'actor' => User::factory()->for($company)->admin()->create(),
+                'subject' => User::factory()->for($company)->create(),
+                'status_code' => 200,
+            ];
+        });
+
+        yield 'Admin for one company should NOT be allowed to update user from another company' => Provider::data(function () {
             [$companyA, $companyB] = Company::factory()->count(2)->create();
 
             return [
+                // @todo:
                 'actor' => User::factory()->for($companyA)->admin()->create(),
                 'subject' => User::factory()->for($companyB)->create(),
                 'status_code' => 403,
             ];
         });
+
+        yield 'Admin for one company should NOT be allowed to update user without a company' => Provider::data(function () {
+            $company = Company::factory()->create();
+
+            return [
+                'actor' => User::factory()->for($company)->admin()->create(),
+                'subject' => User::factory()->create(['company_id' => null]),
+                'status_code' => 403,
+            ];
+        });
+
+        yield 'Admin without a company should be allowed to update user without a company' => Provider::data(function () {
+            return [
+                'actor' => User::factory()->admin()->create(['company_id' => null]),
+                'subject' => User::factory()->create(['company_id' => null]),
+                'status_code' => 403,
+            ];
+        });
+
+        yield 'Admin without a company should NOT be allowed to update user that has a company' => Provider::data(function () {
+            return [
+                'actor' => User::factory()->admin()->create(['company_id' => null]),
+                'subject' => User::factory()->for(Company::factory())->create(),
+                'status_code' => 403,
+            ];
+        });
+
+        // @todo: delete when done referencing
+        // yield 'Admin attempting to update user without a company' => Provider::data(function () {
+        //     [$companyA, $companyB] = Company::factory()->count(2)->create();
+        //
+        //     return [
+        //         'actor' => User::factory()->for($companyA)->admin()->create(),
+        //         'subject' => User::factory()->for($companyB)->create(),
+        //         'status_code' => 403,
+        //     ];
+        // });
     }
 
     /** @dataProvider provider */
