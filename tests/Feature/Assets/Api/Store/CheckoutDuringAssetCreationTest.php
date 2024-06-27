eaf4ebef4f0412ad02b127e9ddcb9c82edbf21cd
Marcus Moore <contact@marcusmoore.io>
2024-06-27 16:01:29 -0700

    Scaffold test cases

--- tests/Feature/Assets/Api/Store/CheckoutDuringAssetCreationTest.php
+++ tests/Feature/Assets/Api/Store/CheckoutDuringAssetCreationTest.php
@@ -89,4 +89,94 @@ class CheckoutDuringAssetCreationTest extends TestCase
         // I think this makes sense, but open to a sanity check
         $this->assertTrue($asset->assignedAssets()->find($response['payload']['id'])->is($apiAsset));
     }
+
+    public function testCanCheckoutAssetToUserViaAssignedToAndAssignedTypeFields()
+    {
+        $this->markTestIncomplete();
+
+        $model = AssetModel::factory()->create();
+        $status = Statuslabel::factory()->create();
+        $user = User::factory()->createAssets()->create();
+        $userAssigned = User::factory()->create();
+
+        $this->settings->enableAutoIncrement();
+
+        $response = $this->actingAsForApi($user)
+            ->postJson(route('api.assets.store'), [
+                'assigned_to' => $userAssigned->id,
+                'assigned_type' => 'user',
+                'model_id' => $model->id,
+                'status_id' => $status->id,
+            ])
+            ->assertOk()
+            ->assertStatusMessageIs('success')
+            ->json();
+
+        $asset = Asset::find($response['payload']['id']);
+
+        $this->assertTrue($asset->adminuser->is($user));
+        $this->assertTrue($asset->checkedOutToUser());
+        $this->assertTrue($asset->assignedTo->is($userAssigned));
+    }
+
+    public function testCanCheckoutAssetToLocationViaAssignedToAndAssignedTypeFields()
+    {
+        $this->markTestIncomplete();
+
+        $model = AssetModel::factory()->create();
+        $status = Statuslabel::factory()->create();
+        $location = Location::factory()->create();
+        $user = User::factory()->createAssets()->create();
+
+        $this->settings->enableAutoIncrement();
+
+        $response = $this->actingAsForApi($user)
+            ->postJson(route('api.assets.store'), [
+                'assigned_to' => $location->id,
+                'assigned_type' => 'location',
+                'model_id' => $model->id,
+                'status_id' => $status->id,
+            ])
+            ->assertOk()
+            ->assertStatusMessageIs('success')
+            ->json();
+
+        $asset = Asset::find($response['payload']['id']);
+
+        $this->assertTrue($asset->adminuser->is($user));
+        $this->assertTrue($asset->checkedOutToLocation());
+        $this->assertTrue($asset->location->is($location));
+    }
+
+    public function testCanCheckoutAssetToAssetViaAssignedToAndAssignedTypeFields()
+    {
+        $this->markTestIncomplete();
+
+        $model = AssetModel::factory()->create();
+        $status = Statuslabel::factory()->create();
+        $asset = Asset::factory()->create();
+        $user = User::factory()->createAssets()->create();
+
+        $this->settings->enableAutoIncrement();
+
+        $response = $this->actingAsForApi($user)
+            ->postJson(route('api.assets.store'), [
+                'assigned_to' => $asset->id,
+                'assigned_type' => 'asset',
+                'model_id' => $model->id,
+                'status_id' => $status->id,
+            ])
+            ->assertOk()
+            ->assertStatusMessageIs('success')
+            ->json();
+
+        $apiAsset = Asset::find($response['payload']['id']);
+
+        $this->assertTrue($apiAsset->adminuser->is($user));
+        $this->assertTrue($apiAsset->checkedOutToAsset());
+        $this->assertTrue($asset->assignedAssets()->find($response['payload']['id'])->is($apiAsset));
+    }
+
+    // @todo: assigned_x not allowed if assigned_to and assigned_type included and vice versa
+    // @todo: ensure user/location/asset exists
 }
