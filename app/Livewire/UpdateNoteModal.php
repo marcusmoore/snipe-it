0a46b01b7031f8f578cc5240d14fd8d09cd11c4c
Marcus Moore <contact@marcusmoore.io>
2025-02-20 16:44:42 -0800

    Allow updating notes

--- app/Livewire/UpdateNoteModal.php
+++ app/Livewire/UpdateNoteModal.php
@@ -3,16 +3,20 @@
 namespace App\Livewire;
 
 use App\Models\Note;
+use Livewire\Attributes\Locked;
 use Livewire\Attributes\On;
 use Livewire\Component;
 
 class UpdateNoteModal extends Component
 {
+    #[Locked]
+    public $noteId;
     public $content;
     #[On('showEditNoteModal')]
     public function showModal($id)
     {
-        $this->content = Note::findOrFail($id)->content;
+        $this->noteId = $id;
+        $this->content = Note::findOrFail($this->noteId)->content;
 
         $this->js("$('#editNoteModal').modal('show')");
     }
@@ -20,6 +24,17 @@ class UpdateNoteModal extends Component
     public function hide()
     {
         $this->js("$('#editNoteModal').modal('hide')");
+    }
+
+    public function save()
+    {
+        $note = Note::findOrFail($this->noteId);
+        $note->content = $this->content;
+        $note->save();
+
+        $this->hide();
+
+        $this->js("$('#assetNotes').bootstrapTable('refresh')");
     }
 
     public function render()
