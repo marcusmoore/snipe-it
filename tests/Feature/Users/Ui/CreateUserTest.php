222990da03159e109eebeeb5e390d4ec7a628a5b
Marcus Moore <contact@marcusmoore.io>
2025-04-10 16:17:54 -0700

    Migrate test to pest

--- tests/Feature/Users/Ui/CreateUserTest.php
+++ tests/Feature/Users/Ui/CreateUserTest.php
@@ -1,20 +1,20 @@
 <?php
 
-namespace Tests\Feature\Users\Ui;
-
 use App\Models\User;
-use Tests\TestCase;
 
-class CreateUserTest extends TestCase
-{
-    public function testPageRenders()
-    {
-        $admin = User::factory()->createUsers()->create();
-        $response = $this->actingAs(User::factory()->superuser()->create())
-            ->get(route('users.create'))
-            ->assertOk();
-        $response->assertDontSee($admin->first_name);
-        $response->assertDontSee($admin->last_name);
-        $response->assertDontSee($admin->email);
-    }
-}
+test('permission required to view create page', function () {
+    $this->actingAs(User::factory()->create())
+        ->get(route('users.create'))
+        ->assertForbidden();
+});
+
+test('create page renders', function () {
+    $admin = User::factory()->createUsers()->create();
+
+    $this->actingAs(User::factory()->superuser()->create())
+        ->get(route('users.create'))
+        ->assertOk()
+        ->assertDontSee($admin->first_name)
+        ->assertDontSee($admin->last_name)
+        ->assertDontSee($admin->email);
+});
