27feebf85e1501bd859fe56088d5dce62f082d9b
Marcus Moore <contact@marcusmoore.io>
2025-04-10 16:22:16 -0700

    Add simple test for creating users

--- tests/Feature/Users/Ui/CreateUserTest.php
+++ tests/Feature/Users/Ui/CreateUserTest.php
@@ -11,10 +11,36 @@ test('permission required to view create page', function () {
 test('create page renders', function () {
     $admin = User::factory()->createUsers()->create();
 
-    $this->actingAs(User::factory()->superuser()->create())
+    $this->actingAs(User::factory()->createUsers()->create())
         ->get(route('users.create'))
         ->assertOk()
         ->assertDontSee($admin->first_name)
         ->assertDontSee($admin->last_name)
         ->assertDontSee($admin->email);
+});
+
+test('permission required to create user', function () {
+    $this->actingAs(User::factory()->create())
+        ->post(route('users.store'), [
+            'first_name' => 'Suki',
+            'username' => 'suki',
+            'password' => 'super-secret',
+            'password_confirmation' => 'super-secret',
+        ])
+        ->assertForbidden();
+});
+
+test('can create user', function () {
+    $this->actingAs(User::factory()->createUsers()->create())
+        ->post(route('users.store'), [
+            'first_name' => 'Suki',
+            'username' => 'suki',
+            'password' => 'super-secret',
+            'password_confirmation' => 'super-secret',
+        ]);
+
+    $this->assertDatabaseHas('users', [
+        'first_name' => 'Suki',
+        'username' => 'suki',
+    ]);
 });
