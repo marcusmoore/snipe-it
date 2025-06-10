585ace5167c188459533fa85bdd4a76c43b6c4cb
Marcus Moore <contact@marcusmoore.io>
2025-06-10 15:12:34 -0700

    Implement some tests

--- tests/Feature/Users/Ui/RestoreUserTest.php
+++ tests/Feature/Users/Ui/RestoreUserTest.php
@@ -2,19 +2,27 @@
 
 namespace Tests\Feature\Users\Ui;
 
+use App\Models\User;
 use Tests\TestCase;
 
 class RestoreUserTest extends TestCase
 {
     public function test_permission_needed_to_restore_user()
     {
-        $this->markTestIncomplete();
+        $trashedUser = User::factory()->trashed()->create();
 
+        $this->actingAs(User::factory()->create())
+            ->post(route('users.restore.store', ['userId' => $trashedUser->id]))
+            ->assertForbidden();
     }
 
     public function test_cannot_restore_non_deleted_user()
     {
-        $this->markTestIncomplete();
+        $nonTrashedUser = User::factory()->create();
+
+        $this->actingAs(User::factory()->deleteUsers()->create())
+            ->post(route('users.restore.store', ['userId' => $nonTrashedUser->id]))
+            ->assertSessionHas('error');
     }
 
     public function test_can_restore_user()
