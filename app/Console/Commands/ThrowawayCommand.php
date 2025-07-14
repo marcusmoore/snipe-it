c9627d6f7d90350b93faeb1d073d05d00a399006
Marcus Moore <contact@marcusmoore.io>
2025-07-14 15:37:13 -0700

    Begin scaffolding command

---
+++ app/Console/Commands/ThrowawayCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Actionlog;
+use App\Models\Asset;
+use App\Models\CheckoutAcceptance;
+use App\Models\Location;
+use Illuminate\Console\Command;
+
+class ThrowawayCommand extends Command
+{
+    protected $signature = 'throwaway';
+
+    protected $description = 'Command description';
+
+    public function handle(): void
+    {
+        // fetch all pending checkout acceptances.
+        // scoping pending works here because locations and assets cannot accept checkouts.
+        $acceptances = CheckoutAcceptance::pending()->get();
+
+        // get action log where the action is "checkout" and the target is Asset or Location.
+        $logs = Actionlog::query()
+            ->where([
+                'action_type' => 'checkout',
+            ])
+            ->whereIn('target_type', [Asset::class, Location::class])
+            ->get();
+
+
+    }
+}
