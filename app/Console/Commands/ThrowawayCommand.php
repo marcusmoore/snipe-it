41da83775c5b873c76b255991da91cef8444796a
Marcus Moore <contact@marcusmoore.io>
2025-07-14 16:08:10 -0700

    Display counts to start

--- app/Console/Commands/ThrowawayCommand.php
+++ app/Console/Commands/ThrowawayCommand.php
@@ -20,6 +20,8 @@ class ThrowawayCommand extends Command
         // scoping pending works here because locations and assets cannot accept checkouts.
         $acceptances = CheckoutAcceptance::pending()->get();
 
+        $this->info("{$acceptances->count()} CheckoutAcceptances retrieved");
+
         // get action log where the action is "checkout" and the target is Asset or Location.
         $logs = Actionlog::query()
             ->where([
@@ -28,15 +30,21 @@ class ThrowawayCommand extends Command
             ->whereIn('target_type', [Asset::class, Location::class])
             ->get();
 
-        /** @var CheckoutAcceptance $testingAcceptance */
-        $testingAcceptance = $acceptances->firstWhere('id', 439);
+        $this->info("{$logs->count()} ActionLogs found");
 
-        $foundLog = $logs->filter(function ($log) use ($testingAcceptance) {
-            return $log->item()->is($testingAcceptance->checkoutable) && $log->created_at->is($testingAcceptance->created_at);
-        });
-
-        dd($foundLog);
+        // /** @var CheckoutAcceptance $testingAcceptance */
+        // $testingAcceptance = $acceptances->firstWhere('id', 439);
+        //
+        // $foundLog = $logs->filter(function ($log) use ($testingAcceptance) {
+        //     return $log->item()->is($testingAcceptance->checkoutable) && $log->created_at->is($testingAcceptance->created_at);
+        // });
+        //
+        // dd($foundLog);
 
         // @todo: now loop through all $acceptances and match them to a $log
+
+        // $acceptances->map(function (CheckoutAcceptance $acceptance) {
+        //
+        // });
     }
 }
