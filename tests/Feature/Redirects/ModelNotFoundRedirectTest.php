fdaa9536dd04117e97bef2584efa1206fffe6337
Marcus Moore <contact@marcusmoore.io>
2025-08-06 14:18:54 -0700

    Convert more tests

--- tests/Feature/Redirects/ModelNotFoundRedirectTest.php
+++ tests/Feature/Redirects/ModelNotFoundRedirectTest.php
@@ -1,51 +1,39 @@
 <?php
 
-namespace Tests\Feature\Redirects;
-
 use App\Models\User;
-use Tests\TestCase;
 
-class ModelNotFoundRedirectTest extends TestCase
-{
-    public function testHandlesAsset404()
-    {
-        $this->actingAs(User::factory()->viewAssets()->create())
-            ->get(route('hardware.checkout.create', 9999))
-            ->assertRedirectToRoute('hardware.index');
-    }
+test('handles asset404', function () {
+    $this->actingAs(User::factory()->viewAssets()->create())
+        ->get(route('hardware.checkout.create', 9999))
+        ->assertRedirectToRoute('hardware.index');
+});
 
-    public function testHandlesAssetMaintenance404()
-    {
-        $this->actingAs(User::factory()->viewAssets()->create())
-            ->get(route('maintenances.show', 9999))
-            ->assertRedirectToRoute('maintenances.index');
-    }
+test('handles asset maintenance404', function () {
+    $this->actingAs(User::factory()->viewAssets()->create())
+        ->get(route('maintenances.show', 9999))
+        ->assertRedirectToRoute('maintenances.index');
+});
 
-    public function testHandlesAssetModel404()
-    {
-        $this->actingAs(User::factory()->viewAssetModels()->create())
-            ->get(route('models.show', 9999))
-            ->assertRedirectToRoute('models.index');
-    }
+test('handles asset model404', function () {
+    $this->actingAs(User::factory()->viewAssetModels()->create())
+        ->get(route('models.show', 9999))
+        ->assertRedirectToRoute('models.index');
+});
 
-    public function testHandlesLicenseSeat404()
-    {
-        $this->actingAs(User::factory()->viewLicenses()->create())
-            ->get(route('licenses.checkin', 9999))
-            ->assertRedirectToRoute('licenses.index');
-    }
+test('handles license seat404', function () {
+    $this->actingAs(User::factory()->viewLicenses()->create())
+        ->get(route('licenses.checkin', 9999))
+        ->assertRedirectToRoute('licenses.index');
+});
 
-    public function testHandlesPredefinedKit404()
-    {
-        $this->actingAs(User::factory()->viewPredefinedKits()->create())
-            ->get(route('kits.show', 9999))
-            ->assertRedirectToRoute('kits.index');
-    }
+test('handles predefined kit404', function () {
+    $this->actingAs(User::factory()->viewPredefinedKits()->create())
+        ->get(route('kits.show', 9999))
+        ->assertRedirectToRoute('kits.index');
+});
 
-    public function testHandlesReportTemplate404()
-    {
-        $this->actingAs(User::factory()->canViewReports()->create())
-            ->get(route('report-templates.show', 9999))
-            ->assertRedirectToRoute('reports/custom');
-    }
-}
+test('handles report template404', function () {
+    $this->actingAs(User::factory()->canViewReports()->create())
+        ->get(route('report-templates.show', 9999))
+        ->assertRedirectToRoute('reports/custom');
+});
