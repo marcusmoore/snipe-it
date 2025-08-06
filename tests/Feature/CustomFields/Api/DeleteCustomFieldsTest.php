87ec98006aed7c57ac0d0ef56095c7dfab8b20ea
Marcus Moore <contact@marcusmoore.io>
2025-08-06 13:18:30 -0700

    Convert more tests

--- tests/Feature/CustomFields/Api/DeleteCustomFieldsTest.php
+++ tests/Feature/CustomFields/Api/DeleteCustomFieldsTest.php
@@ -1,54 +1,45 @@
 <?php
 
-namespace Tests\Feature\CustomFields\Api;
-
 use App\Models\CustomField;
 use App\Models\CustomFieldset;
 use App\Models\User;
 use Tests\Concerns\TestsPermissionsRequirement;
-use Tests\TestCase;
 
-class DeleteCustomFieldsTest extends TestCase implements TestsPermissionsRequirement
-{
-    public function testRequiresPermission()
-    {
-        $this->markIncompleteIfMySQL('Custom Fields tests do not work on MySQL');
+test('requires permission', function () {
+    $this->markIncompleteIfMySQL('Custom Fields tests do not work on MySQL');
 
-        $customField = CustomField::factory()->create();
+    $customField = CustomField::factory()->create();
 
-        $this->actingAsForApi(User::factory()->create())
-            ->deleteJson(route('api.customfields.destroy', $customField))
-            ->assertForbidden();
+    $this->actingAsForApi(User::factory()->create())
+        ->deleteJson(route('api.customfields.destroy', $customField))
+        ->assertForbidden();
 
-        $this->assertDatabaseHas('custom_fields', ['id' => $customField->id]);
-    }
+    $this->assertDatabaseHas('custom_fields', ['id' => $customField->id]);
+});
 
-    public function testCustomFieldsCannotBeDeletedIfTheyHaveAssociatedFieldsets()
-    {
-        $this->markIncompleteIfMySQL('Custom Fields tests do not work on MySQL');
+test('custom fields cannot be deleted if they have associated fieldsets', function () {
+    $this->markIncompleteIfMySQL('Custom Fields tests do not work on MySQL');
 
-        $customField = CustomField::factory()->create();
-        $customFieldset = CustomFieldset::factory()->create();
+    $customField = CustomField::factory()->create();
+    $customFieldset = CustomFieldset::factory()->create();
 
-        $customField->fieldset()->attach($customFieldset, ['order' => 1, 'required' => 'false']);
+    $customField->fieldset()->attach($customFieldset, ['order' => 1, 'required' => 'false']);
 
-        $this->actingAsForApi(User::factory()->deleteCustomFields()->create())
-            ->deleteJson(route('api.customfields.destroy', $customField))
-            ->assertStatusMessageIs('error');
+    $this->actingAsForApi(User::factory()->deleteCustomFields()->create())
+        ->deleteJson(route('api.customfields.destroy', $customField))
+        ->assertStatusMessageIs('error');
 
-        $this->assertDatabaseHas('custom_fields', ['id' => $customField->id]);
-    }
+    $this->assertDatabaseHas('custom_fields', ['id' => $customField->id]);
+});
 
-    public function testCustomFieldsCanBeDeleted()
-    {
-        $this->markIncompleteIfMySQL('Custom Fields tests do not work on MySQL');
+test('custom fields can be deleted', function () {
+    $this->markIncompleteIfMySQL('Custom Fields tests do not work on MySQL');
 
-        $customField = CustomField::factory()->create();
+    $customField = CustomField::factory()->create();
 
-        $this->actingAsForApi(User::factory()->deleteCustomFields()->create())
-            ->deleteJson(route('api.customfields.destroy', $customField))
-            ->assertStatusMessageIs('success');
+    $this->actingAsForApi(User::factory()->deleteCustomFields()->create())
+        ->deleteJson(route('api.customfields.destroy', $customField))
+        ->assertStatusMessageIs('success');
 
-        $this->assertDatabaseMissing('custom_fields', ['id' => $customField->id]);
-    }
-}
+    $this->assertDatabaseMissing('custom_fields', ['id' => $customField->id]);
+});
