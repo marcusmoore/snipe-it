d6940d7322095a34e675278353a35a7debb48fbd
Marcus Moore <contact@marcusmoore.io>
2025-08-06 15:48:17 -0700

    Use base TestCase in unit tests

--- tests/Pest.php
+++ tests/Pest.php
@@ -11,9 +11,8 @@
 |
 */
 
-pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
-    ->in('Feature');
+pest()->extend(Tests\TestCase::class)->in('Feature');
+pest()->extend(Tests\TestCase::class)->in('Unit');
 
 /*
 |--------------------------------------------------------------------------
