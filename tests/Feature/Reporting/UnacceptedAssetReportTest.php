a7378c0f648d5f5f80b6d518cb677077186d2d6f
Marcus Moore <contact@marcusmoore.io>
2025-08-06 14:20:08 -0700

    Convert more tests

--- tests/Feature/Reporting/UnacceptedAssetReportTest.php
+++ tests/Feature/Reporting/UnacceptedAssetReportTest.php
@@ -1,61 +1,46 @@
 <?php
 
-namespace Tests\Feature\Reporting;
-
-use App\Models\Asset;
-use App\Models\Company;
 use App\Models\User;
 use Illuminate\Testing\TestResponse;
 use League\Csv\Reader;
 use PHPUnit\Framework\Assert;
-use Tests\TestCase;
 
-class UnacceptedAssetReportTest extends TestCase
-{
-    protected function setUp(): void
-    {
-        parent::setUp();
+beforeEach(function () {
+    TestResponse::macro(
+        'assertSeeTextInStreamedResponse',
+        function (string $needle) {
+            Assert::assertTrue(
+                collect(Reader::createFromString($this->streamedContent())->getRecords())
+                    ->pluck(0)
+                    ->contains($needle)
+            );
 
-        TestResponse::macro(
-            'assertSeeTextInStreamedResponse',
-            function (string $needle) {
-                Assert::assertTrue(
-                    collect(Reader::createFromString($this->streamedContent())->getRecords())
-                        ->pluck(0)
-                        ->contains($needle)
-                );
+            return $this;
+        }
+    );
 
-                return $this;
-            }
-        );
+    TestResponse::macro(
+        'assertDontSeeTextInStreamedResponse',
+        function (string $needle) {
+            Assert::assertFalse(
+                collect(Reader::createFromString($this->streamedContent())->getRecords())
+                    ->pluck(0)
+                    ->contains($needle)
+            );
 
-        TestResponse::macro(
-            'assertDontSeeTextInStreamedResponse',
-            function (string $needle) {
-                Assert::assertFalse(
-                    collect(Reader::createFromString($this->streamedContent())->getRecords())
-                        ->pluck(0)
-                        ->contains($needle)
-                );
+            return $this;
+        }
+    );
+});
 
-                return $this;
-            }
-        );
-    }
+test('permission required to view unaccepted asset report', function () {
+    $this->actingAs(User::factory()->create())
+        ->get(route('reports/unaccepted_assets'))
+        ->assertForbidden();
+});
 
-
-    public function testPermissionRequiredToViewUnacceptedAssetReport()
-    {
-        $this->actingAs(User::factory()->create())
-            ->get(route('reports/unaccepted_assets'))
-            ->assertForbidden();
-    }
-
-    public function testUserCanListUnacceptedAssets()
-    {
-        $this->actingAs(User::factory()->superuser()->create())
-            ->get(route('reports/unaccepted_assets'))
-            ->assertOk();
-    }
-    
-}
+test('user can list unaccepted assets', function () {
+    $this->actingAs(User::factory()->superuser()->create())
+        ->get(route('reports/unaccepted_assets'))
+        ->assertOk();
+});
