30e99903a795ddbbda9f2b7f1a8b622f8e31f54a
Marcus Moore <contact@marcusmoore.io>
2025-10-30 14:05:57 -0700

    Add todo

--- app/Actions/DeleteFile.php
+++ app/Actions/DeleteFile.php
@@ -8,6 +8,7 @@ class DeleteFile
 {
     public static function run($path, $disk = 'public')
     {
+        // @todo: add try / catch and log if exception thrown
         Storage::disk($disk)->delete($path);
     }
 }
