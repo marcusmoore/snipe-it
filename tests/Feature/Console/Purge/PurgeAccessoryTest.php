ca33d442b0f1d09784658412f49cd3341de2a31e
Marcus Moore <contact@marcusmoore.io>
2025-11-18 17:23:02 -0800

    Populate failing tests

--- tests/Feature/Console/Purge/PurgeAccessoryTest.php
+++ tests/Feature/Console/Purge/PurgeAccessoryTest.php
@@ -36,7 +36,45 @@ class PurgeAccessoryTest extends TestCase
 
     public function test_associated_action_logs_are_not_purged_by_default()
     {
-        $this->markTestIncomplete();
+        // $this->markTestIncomplete();
+
+        $accessory = Accessory::factory()->create();
+
+        $originalCount = DB::table('action_logs')
+            ->where([
+                'item_type' => Accessory::class,
+                'item_id' => $accessory->id,
+            ])
+            ->count();
+
+        $this->assertGreaterThan(
+            0,
+            $originalCount,
+            'Model does not have action log entries as expected'
+        );
+
+        $accessory->delete();
+
+        $this->firePurgeCommand()->assertSuccessful();
+
+        $newCount = DB::table('action_logs')
+            ->where([
+                'item_type' => Accessory::class,
+                'item_id' => $accessory->id,
+            ])
+            ->whereNull('deleted_at')
+            ->count();
+
+        // all entries should be soft deleted including the last "deleted" entry
+        $this->assertEquals($originalCount + 1, $newCount);
+    }
+
+    public function test_associated_action_logs_can_be_purged_via_env_variable()
+    {
+        // $this->markTestIncomplete();
+
+        // todo: build out this variable
+        config(['app.include_action_logs_when_purging', true]);
 
         $accessory = Accessory::factory()->create();
 
@@ -64,16 +102,9 @@ class PurgeAccessoryTest extends TestCase
             ])
             ->count();
 
-        // should include the "delete" entry
-        $this->assertEquals($originalCount + 1, $newCount);
-    }
-
-    public function test_associated_action_logs_can_be_purged_via_env_variable()
-    {
-        $this->markTestIncomplete();
-
         // todo: should this only be the final "delete" or nothing at all?
         // Current behavior is one "delete" entry
+        $this->assertEquals($originalCount + 1, $newCount);
     }
 
     public function test_deletes_accessories_image()
