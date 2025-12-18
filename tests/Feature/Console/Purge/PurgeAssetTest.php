0b481084044a780954a2af24083c70909441d14b
Marcus Moore <contact@marcusmoore.io>
2025-12-17 16:59:27 -0800

    Scaffold test

--- tests/Feature/Console/Purge/PurgeAssetTest.php
+++ tests/Feature/Console/Purge/PurgeAssetTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature\Console\Purge;
 
 use App\Models\Asset;
 use App\Models\Maintenance;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 use PHPUnit\Framework\Attributes\Group;
 use Tests\Feature\Console\Purge\Traits\FiresPurgeCommand;
@@ -52,9 +54,70 @@ class PurgeAssetTest extends TestCase
     public function test_associated_action_logs_are_not_purged_by_default()
     {
         $this->markTestIncomplete();
+
+        $asset = Asset::factory()->create();
+
+        $query = DB::table('action_logs')->where([
+            'item_type' => Asset::class,
+            'item_id' => $asset->id,
+        ]);
+
+        $originalCount = $query->count();
+
+        $this->assertGreaterThan(
+            0,
+            $originalCount,
+            'Model does not have action log entries as expected'
+        );
+
+        $asset->delete();
+
+        $this->firePurgeCommand()->assertSuccessful();
+
+        $newCount = $query
+            ->whereNotNull('deleted_at')
+            ->count();
+
+        // all entries should be soft deleted including the "delete" and "force delete" entries
+        $this->assertEquals($originalCount + 2, $newCount);
     }
 
     public function test_associated_action_logs_can_be_purged_via_env_variable()
+    {
+        $this->markTestIncomplete();
+
+        Config::set('app.include_related_action_logs_when_purging', true);
+
+        $asset = Asset::factory()->create();
+
+        $query = DB::table('action_logs')->where([
+            'item_type' => Asset::class,
+            'item_id' => $asset->id,
+        ]);
+
+        $originalCount = $query->count();
+
+        $this->assertGreaterThan(
+            0,
+            $originalCount,
+            'Model does not have action log entries as expected'
+        );
+
+        $asset->delete();
+
+        $this->firePurgeCommand()->assertSuccessful();
+
+        $newCount = $query->count();
+
+        $this->assertEquals(0, $newCount);
+    }
+
+    public function test_associated_action_logs_for_maintenances_are_not_purged_by_default()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function test_associated_action_logs_for_maintenances_can_be_purged_via_env_variable()
     {
         $this->markTestIncomplete();
     }
