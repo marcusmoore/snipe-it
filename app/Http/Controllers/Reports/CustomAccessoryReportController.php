3e5c2cd5553cd0430ee887ff7b80a263d2aee964
Marcus Moore <contact@marcusmoore.io>
2026-04-28 14:18:57 -0700

    Disable debugbar when generating report

--- app/Http/Controllers/Reports/CustomAccessoryReportController.php
+++ app/Http/Controllers/Reports/CustomAccessoryReportController.php
@@ -34,5 +34,8 @@ class CustomAccessoryReportController extends Controller
     {
         ini_set('max_execution_time', env('REPORT_TIME_LIMIT', 12000)); // 12000 seconds = 200 minutes
         $this->authorize('reports.view');
+
+        $this->disableDebugbar();
+
     }
 }
