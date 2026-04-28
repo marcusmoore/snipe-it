5275ddbd6b45b76a4d76b67bb58c303c0b5919d1
Marcus Moore <contact@marcusmoore.io>
2026-04-28 13:21:00 -0700

    WIP: begin to build out report page

--- app/Http/Controllers/Reports/CustomAccessoryReportController.php
+++ app/Http/Controllers/Reports/CustomAccessoryReportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Reports;
 
 use App\Http\Controllers\Controller;
+use App\Models\ReportTemplate;
 use Illuminate\Http\Request;
 
 class CustomAccessoryReportController extends Controller
@@ -11,7 +12,22 @@ class CustomAccessoryReportController extends Controller
     {
         $this->authorize('reports.view');
 
-        return view('reports.custom.accessory');
+        $report_templates = ReportTemplate::where('type', 'accessory')->orderBy('name')->get();
+
+        // The view needs a template to render correctly, even if it is empty...
+        $template = new ReportTemplate;
+
+        // Set the report's input values in the cases we were redirected back
+        // with validation errors so the report is populated as expected.
+        if ($request->old()) {
+            $template->name = $request->old('name');
+            $template->options = $request->old();
+        }
+
+        return view('reports.custom.accessory', [
+            'report_templates' => $report_templates,
+            'template' => $template,
+        ]);
     }
 
     public function run() {}
