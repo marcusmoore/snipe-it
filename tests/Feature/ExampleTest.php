54b2f5b6605f3b6bfa9d1252ad491f1ac0c9c9f6
Marcus Moore <contact@marcusmoore.io>
2026-05-21 14:34:03 -0700

    Add login test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,13 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+use App\Models\User;
 
-    $response->assertStatus(200);
+it('can login', function () {
+    User::factory()->create(['username' => 'marcus', 'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi']);
+
+    visit('/login')->assertSee('Please Login')
+        ->type('username', 'marcus')
+        ->type('password', 'password')
+        ->click('Login')
+        ->assertSee('You have successfully logged in');
 });
