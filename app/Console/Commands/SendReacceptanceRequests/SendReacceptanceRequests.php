364a38e8468a937afc14ad4a09cf75c5039b68e6
Marcus Moore <contact@marcusmoore.io>
2026-06-29 14:08:33 -0700

    Acceptance: move command to own directory

--- app/Console/Commands/SendReacceptanceRequests/SendReacceptanceRequests.php
+++ app/Console/Commands/SendReacceptanceRequests/SendReacceptanceRequests.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\SendReacceptanceRequests;
 
 use App\Actions\Acceptances\CreateCheckoutAcceptanceAction;
 use App\Enums\ActionType;
